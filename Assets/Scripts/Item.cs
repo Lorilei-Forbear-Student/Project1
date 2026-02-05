439c53896e345ed94c78af7450e5619b77ffb5b6
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-02-05 08:45:19 -0500

    finished item script via garnetkane99 on github

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+//taken from GarnetKane99 on github
+[CreateAssetMenu(fileName = "Item", menuName = "ScriptableObjects/Items")]
+public class Item : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string id;
+    public Sprite icon;
+    public int itemAmount;
+    public int itemCost;
+}
 
-    // Update is called once per frame
-    void Update()
+public static class ScriptableObjectExtension
+{
+    public static T Clone<T>(this T scriptableObject) where T : ScriptableObject
     {
-        
+        if(scriptableObject == null)
+        {
+            Debug.LogError($"error: null scriptable object. returning default {typeof(T)} object.");
+            return (T)ScriptableObject.CreateInstance(typeof(T));
+        }
+
+        T instance = UnityEngine.Object.Instantiate(scriptableObject);
+        instance.name = scriptableObject.name;
+        return instance;
     }
 }
