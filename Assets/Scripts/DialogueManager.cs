5be94d6af2fb333ac459362e3816d1f8c1225dfd
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-02-12 09:05:00 -0500

    for some reason didnt save scene? new branch

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    private int index;
     // Start is called before the first frame update
     void Start()
     {
