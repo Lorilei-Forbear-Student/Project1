3c9b7befab7af5c18dbe6b10a3bb52b081abff2e
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-02-16 09:56:33 -0500

    fixed game over & menu toggles, closing ui branch

--- Assets/Scripts/ToggleMenu.cs
+++ Assets/Scripts/ToggleMenu.cs
@@ -1,22 +1,38 @@
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class ToggleMenu : MonoBehaviour
 {
-    public GameObject menu;
-    public bool menuUp = false;
+    public GameObject menu, map, inventory;
+    public bool menuUp, mapUp, invUp = false;
 
 
     void Update()
     {
-        if (Input.GetKeyDown("q")) menuUp = !menuUp;
-
-        menu.SetActive(menuUp);
-        PauseGame();
+        if (Input.GetKeyDown("escape") && invUp == false && mapUp == false)
+            {
+                menuUp = !menuUp;
+                menu.SetActive(menuUp);
+            }
+        if (Input.GetKeyDown("q") && menuUp == false && mapUp == false)
+            {
+                invUp = !invUp;
+                inventory.SetActive(invUp);
+            }
+        
+        if (Input.GetKeyDown("m") && menuUp == false && invUp == false)
+            {
+                mapUp = !mapUp;
+                map.SetActive(mapUp);
+            }
+            
+            PauseGame();
     }
 
     void PauseGame()
     {
-        if(menuUp == true)
+        if(menuUp == true || mapUp == true || invUp == true)
         {
             Time.timeScale = 0f;
         }
