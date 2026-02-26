cc95f36f395e03f3eca32896db888ecb3c8870b8
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-02-26 09:25:30 -0500

    followed tutorial, need to fix duplication glitch

---
+++ Assets/Scripts/UISlotHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+public class UISlotHandler : MonoBehaviour, IPointerClickHandler
+{
+    public Item item;
+    public Image icon;
+    public TextMeshProUGUI itemCountText;
+    public InventoryManager inventoryManager;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+        if(eventData.button == PointerEventData.InputButton.Right)
+        {
+            if(item == null) {return;}
+
+            MouseManager.instance.PickupFromStack(this);
+            return;
+        }
+
+        MouseManager.instance.UpdateHeldItem(this);
+        return;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (item != null)
+        {
+            item = item.Clone();
+            icon.sprite = item.itemIcon;
+            itemCountText.text = item.itemCount.ToString();
+        }
+        else
+        {
+            icon.gameObject.SetActive(false);
+            itemCountText.text = string.Empty;
+        }
+    }
+}
