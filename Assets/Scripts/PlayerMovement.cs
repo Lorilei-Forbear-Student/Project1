10da1d4585e6a5af7b00e5e4df1c4964cb9bf4d5
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-03-02 10:35:22 -0500

    walking and idle animations done and working yayyy

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,11 @@ public class PlayerMovement : MonoBehaviour
     private bool isFacingRight = true;
     private Vector2 movement;
     [SerializeField] private Rigidbody2D rb;
-
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -23,6 +27,14 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = movement * speed;
+        if(rb.velocity.magnitude != 0f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 
 
