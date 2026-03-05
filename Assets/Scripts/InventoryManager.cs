d799798ec9d38b5b0646632db819c49d7ff3f347
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-03-05 09:52:54 -0500

    working on inv3, script not done, but working. :(

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    [SerializeField] public List<GameObject> container;
+    List<GameObject> inventory = new List<GameObject>();
+    public UISlotHandler currentSlot;
+    public Item item;
+
+    void Awake()
+    {
+        EstablishInventory();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("item"))
+        {
+            
+            //when available slot (to come) == currentSlot
+            //how to get item?
+            PlaceInInventory(currentSlot, item);
+        }
+    }
+    public List<GameObject> EstablishInventory()
+    {
+        
+        foreach(GameObject slot in container)
+        {
+            inventory.Add(slot);
+            Debug.Log("slot added");
+        }
+        return inventory;
+    }
     public void StackInInventory(UISlotHandler currentSlot, Item item)
     {
         if(currentSlot.item.itemId != item.itemId) { return; }
 
         currentSlot.item.itemCount += item.itemCount;
         currentSlot.itemCountText.text = currentSlot.item.itemCount.ToString();
-        //ConfigureInventory();
     }
     public void PlaceInInventory(UISlotHandler currentSlot, Item item)
     {
