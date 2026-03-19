8b41df0f4934013b1e56cf5781ac0a695ead476d
Lorilei-Forbear-Student <207830@student.oaisd.org>
2026-03-19 10:12:39 -0400

    continuation of inv rehash, and some maintenance

--- Assets/Scripts/ToggleMenu.cs
+++ Assets/Scripts/ToggleMenu.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class ToggleMenu : MonoBehaviour
 {
-    public GameObject menu, map;
-    public bool menuUp, mapUp = false;
+    public GameObject menu, map, inv;
+    public bool menuUp, mapUp, invUp = false;
 
 
     void Update()
     {
-        if (Input.GetKeyDown("escape") && mapUp == false)
+        if (Input.GetKeyDown("escape") && mapUp == false && invUp == false)
             {
                 menuUp = !menuUp;
                 menu.SetActive(menuUp);
             }
         
-        if (Input.GetKeyDown("m") && menuUp == false)
+        if (Input.GetKeyDown("m") && menuUp == false && invUp == false)
             {
                 mapUp = !mapUp;
                 map.SetActive(mapUp);
+            }
+        if (Input.GetKeyDown("e") && menuUp == false && mapUp == false)
+            {
+                invUp = !invUp;
+                inv.SetActive(invUp);
             }
             
             PauseGame();
@@ -27,7 +32,7 @@ public class ToggleMenu : MonoBehaviour
 
     void PauseGame()
     {
-        if(menuUp == true || mapUp == true)
+        if(menuUp == true || mapUp == true || invUp == true)
         {
             Time.timeScale = 0f;
         }
